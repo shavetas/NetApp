ec9f19da4c1f7e1be62a9b1297a252de6ed7f11b
hiteshkr759 <hiteshkr759@gmail.com>
2018-08-13 11:35:15 +0530

    Css Added Fix

---
+++ Search Page.java
@@ -0,0 +1,10 @@
+<quilt layout="one-column" nestable="false" disableTopCssClass="false">
+  <add to="main-content">
+    <component id="custom-search-angular"/>
+    <component id="custom-css-search"/>
+    <component id="custom.grz.detailSearch"/>
+  </add>
+  <remove from="site-navigation" for="Header">
+    <component id="common.widget.search-form-message"/>
+  </remove>
+</quilt>
